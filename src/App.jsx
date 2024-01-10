b626204b81e76a99fe8492e85faf41cf5e9d5f12
Nihal-Abedin <nihal.abedin.annon99@gmail.com>
2024-01-11 04:26:04 +0600

    card

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <div className="cont">
+      <div className="image">
+        <img src="../src/assets/left2.jpeg" className="image1" alt="image" />
+        <img src="../src/assets/left2.jpeg" className="image3" alt="image" />
+        <img src="../src/assets/right2.jpeg" className="image2" alt="image" />
+      </div>
+      <div className="name-plate">
+        <div className="names">
+          <h1>Nihal</h1>
+          <p>&</p>
+          <h2>Fabiha's</h2>
+        </div>
+        <h3 className="punch-line">Reception</h3>
+        <h3 className="date">
+          ON 3rd FEBRUARY 2024 <br /> JATRABARI CONVENTION CENTER, DHAKA
+        </h3>
+        <p className="tag">Bless the married couple for a happy life!</p>
+        <p className="dance">dinner & dancing to follow</p>
+        <div className="actions">
+          <button>See the venue</button>
+        </div>
+        <p className="footer">
+          Can't wait to celebrate auspicious moment of our family with you!
+          <br /> Just a ping away for any queries: +8801957009313
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
